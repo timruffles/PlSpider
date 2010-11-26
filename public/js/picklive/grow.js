d0e2255d0aaad5257ff44e9fe1a09940b2c570ad
Tim Ruffles <timr@picklive.com>
2010-11-26 16:49:44 +0000

    With some rudimentory data countage

--- public/js/picklive/grow.js
+++ public/js/picklive/grow.js
@@ -15,18 +15,29 @@ $(document).ready(function() {
         var name = setup[1],
             players = setup[0];
             
+            
         players.forEach(function(playerName) {
           // FIXME obviously not great to use lots of the same ids...
           svg.add($('#kit'))
           var id  = _.uniqueId(),
-              kit = svg.getElementById('kit');
-          kit.setAttribute('id',id);
+              kit = svg.getElementById('kit'),
+              dataTable = $('#dataTable').clone().attr('id',_.uniqueId());
+              
+          $('#svgBase').append(dataTable);
           
+          kit.setAttribute('id',id);
           kit.setAttribute('x',rowX);
           kit.setAttribute('y',rowY)
           //svg.rect(rowX + 10,rowY + 100,60,100,{fill:'none',stroke:'blue'})
-          rowX += 80;
           playerData[playerName]['element'] = kit;
+          playerData[playerName]['dataTable'] = dataTable;
+          
+          dataTable.css({
+            display:'block',
+            left: rowX - 25  + 'px',
+            top:  rowY + 100 + 'px'
+          });
+          rowX += 80;
         })
         rowY += 250;
         rowX = 0;
@@ -50,6 +61,10 @@ $(document).ready(function() {
       }
     }
     $('#action').html(evt.action);
+    if(['pass','interception','foul'].indexOf(evt.action) != -1) {
+      var dataPoint = $(playerData[evt.player].dataTable).find('.' + evt.action + ' .data');
+      dataPoint.html(Number(dataPoint.html()) + 1);
+    }
     $(playerData[evt.player].element).animate({
       svgOpacity: 0.5
     },500, function(){
